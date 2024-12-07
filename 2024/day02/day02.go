502c83ddf8ef5ab9e1774490aef3d1319a265381
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-12-07 11:06:44 -0800

    feat: day02 part 1 is done ✅

--- 2024/day02/day02.go
+++ 2024/day02/day02.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Advent of Code - Day 01")
@@ -9,5 +16,99 @@ func main() {
 }
 
 func day02(filename string) int {
-	return 0
+	// Open the file and prepare for iterating over it.
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("err opening filename: %s", err.Error())
+	}
+	defer func(file *os.File) {
+		err = file.Close()
+		if err != nil {
+			log.Fatalf("err while closing filename: %s", err.Error())
+		}
+	}(file)
+
+	// Prepare some variables to keep track of things, and iterate over the scanner.
+	rowIsSafe := make([]bool, 0, 10)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		parts := make([]int, 0, 10)
+		for _, field := range strings.Fields(scanner.Text()) {
+			var num int // Declare this here so `err` isn't a shadowed variable.
+			num, err = strconv.Atoi(field)
+			if err != nil {
+				log.Fatalf("error converting string to int: %s", err)
+			}
+			parts = append(parts, num)
+		}
+		partsLen := len(parts)
+
+		// If numbers are ascending
+		if parts[0] < parts[1] {
+			for i := 0; i < partsLen-1; i++ {
+				// If order isn't right, fail.
+				if parts[i] > parts[i+1] {
+					rowIsSafe = append(rowIsSafe, false)
+					break
+				}
+				// If the numbers are the same, fail.
+				if parts[i] == parts[i+1] {
+					rowIsSafe = append(rowIsSafe, false)
+					break
+				}
+				// Numbers can only have a difference of 1, 2, or 3.
+				if abs(parts[i]-parts[i+1]) > 3 {
+					rowIsSafe = append(rowIsSafe, false)
+					break
+				}
+				// If we made it this far, it's a safe row.
+				if i == partsLen-2 {
+					rowIsSafe = append(rowIsSafe, true)
+				}
+			}
+		} else if parts[0] > parts[1] { // If numbers are descending
+			for i := 0; i < partsLen-1; i++ {
+				// If order isn't right, fail.
+				if parts[i] < parts[i+1] {
+					rowIsSafe = append(rowIsSafe, false)
+					break
+				}
+				// If the numbers are the same, fail.
+				if parts[i] == parts[i+1] {
+					rowIsSafe = append(rowIsSafe, false)
+					break
+				}
+				// Numbers can only have a difference of 1, 2, or 3.
+				if abs(parts[i]-parts[i+1]) > 3 {
+					rowIsSafe = append(rowIsSafe, false)
+					break
+				}
+				// If we made it this far, it's a safe row.
+				if i == partsLen-2 {
+					rowIsSafe = append(rowIsSafe, true)
+				}
+			}
+		} else {
+			// Numbers are the same
+			rowIsSafe = append(rowIsSafe, false)
+		}
+	}
+
+	safeReports := 0
+	for _, v := range rowIsSafe {
+		if v == true {
+			safeReports += 1
+		}
+	}
+
+	return safeReports
+}
+
+// abs returns the absolute version of an int.
+// For example, -4 => 4 and 5 => 5.
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
--- 2024/day02/day02_test.go
+++ 2024/day02/day02_test.go
@@ -11,3 +11,38 @@ func TestDay02(t *testing.T) {
 		t.Errorf("got: %d, want: %d", got, want)
 	}
 }
+
+func TestAbs(t *testing.T) {
+	tests := map[string]struct {
+		input int
+		want  int
+	}{
+		"negative int": {
+			input: -5,
+			want:  5,
+		},
+		"positive int": {
+			input: 5,
+			want:  5,
+		},
+		"zero": {
+			input: 0,
+			want:  0,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := abs(test.input)
+			if got != test.want {
+				t.Errorf("got: %d, want: %d", got, test.want)
+			}
+		})
+	}
+}
+
+func BenchmarkDay02(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		day02(filename)
+	}
+}
