97e889181406ecb9525bf05be3d29ce28bb97de7
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-12-06 16:27:18 -0800

    tests: beef up tests
    Add tests for new function.
    
    Add benchmark test.

--- 2024/day01/day01_test.go
+++ 2024/day01/day01_test.go
@@ -2,10 +2,47 @@ package main
 
 import "testing"
 
+var filename = "day01_input_test.txt"
+
 func TestDay01(t *testing.T) {
-	got := day01("input_test.txt")
+	got := day01(filename)
 	want := 11
 	if got != want {
-		t.Errorf("got: %d, want %d", got, want)
+		t.Errorf("got: %d, want: %d", got, want)
+	}
+}
+
+func TestAbs(t *testing.T) {
+	tests := map[string]struct {
+		input int
+		want  int
+	}{
+		"negative int": {
+			input: -5,
+			want:  5,
+		},
+		"positive int": {
+			input: 5,
+			want:  5,
+		},
+		"zero": {
+			input: 0,
+			want:  0,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := abs(test.input)
+			if got != test.want {
+				t.Errorf("got: %d, want: %d", got, test.want)
+			}
+		})
+	}
+}
+
+func BenchmarkDay01(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		day01(filename)
 	}
 }
