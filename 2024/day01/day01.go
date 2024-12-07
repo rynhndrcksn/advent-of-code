5a6820df55d167d527a2129455338f6ab8b92d31
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-12-06 18:19:08 -0800

    feat: day01 part 2 is now done 🎉

--- 2024/day01/day01.go
+++ 2024/day01/day01.go
@@ -12,10 +12,13 @@ import (
 
 func main() {
 	fmt.Println("Advent of Code - Day 01")
-	fmt.Println("Part 1 Result:", day01("day01_input.txt"))
+	distance, similarity := day01("day01_input.txt")
+	fmt.Println("Part 1 Result:", distance)
+	fmt.Println("Part 2 Results:", similarity)
 }
 
-func day01(fileName string) int {
+func day01(fileName string) (int, int) {
+	// Open the file and prepare for iterating over it.
 	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatalf("err opening filename: %s", err.Error())
@@ -27,26 +30,42 @@ func day01(fileName string) int {
 		}
 	}(file)
 
-	// Create a scanner and add each column to their own slices.
-	col1 := make([]int, 0, 10)
-	col2 := make([]int, 0, 10)
+	// Prepare some variables to keep track of things, and iterate over the scanner.
+	col1Ints := make([]int, 0, 10)
+	col2Ints := make([]int, 0, 10)
+	col1Occurrences := make(map[int]int)
+	col2Occurrences := make(map[int]int)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		parts := strings.Split(scanner.Text(), "   ")
 
+		// Parse the first int and add it to the col1Ints slice.
 		var num1 int
 		num1, err = strconv.Atoi(parts[0])
 		if err != nil {
 			log.Fatalf("err converting parts[0] to int: %s", parts[0])
 		}
-		col1 = append(col1, num1)
+		col1Ints = append(col1Ints, num1)
 
+		// Parse the second int and add it to the col2Ints slice.
 		var num2 int
 		num2, err = strconv.Atoi(parts[1])
 		if err != nil {
 			log.Fatalf("err converting parts[1] to int: %s", parts[1])
 		}
-		col2 = append(col2, num2)
+		col2Ints = append(col2Ints, num2)
+
+		// Update the maps tracking number occurrences.
+		if _, ok := col1Occurrences[num1]; ok {
+			col1Occurrences[num1] += 1
+		} else {
+			col1Occurrences[num1] = 1
+		}
+		if _, ok := col2Occurrences[num2]; ok {
+			col2Occurrences[num2] += 1
+		} else {
+			col2Occurrences[num2] = 1
+		}
 	}
 
 	// Make sure scanner didn't encounter any issues.
@@ -54,17 +73,26 @@ func day01(fileName string) int {
 		log.Fatalf("err from bufio scanner: %s", err.Error())
 	}
 
-	// Sort each column
-	slices.Sort(col1)
-	slices.Sort(col2)
+	// Sort each column.
+	slices.Sort(col1Ints)
+	slices.Sort(col2Ints)
 
-	// Iterate over the slices and grab the total.
-	var total int
-	for k := range col1 {
-		total += abs(col1[k] - col2[k])
+	// Compute similarity score.
+	var similarity int
+	for k, v := range col1Occurrences {
+		num := col2Occurrences[k]
+		for i := 0; i < v; i++ {
+			similarity += k * num
+		}
 	}
 
-	return total
+	// Iterate over the slices and grab the distance.
+	var distance int
+	for k := range col1Ints {
+		distance += abs(col1Ints[k] - col2Ints[k])
+	}
+
+	return distance, similarity
 }
 
 // abs returns the absolute version of an int.
